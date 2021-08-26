71eccddf61a09003ffc9d3b25c6f8c48e4c0ca6f
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-08-26 13:41:15 +0200

    Initial commit

---
+++ src/Studsboll.java
@@ -0,0 +1,18 @@
+/**
+ * Skapat augusti 2021-08-26 - 13:23
+ * Studsboll
+ * Skriven av coolingen
+ */
+public class Studsboll {
+    public static void main(String[] args) {
+         double t = 0;
+         double y = 0;
+         double a = 9.82;
+
+        while (boll studsa take t and change every 1 sec){
+
+        }
+
+        System.out.println("[T=" +t+ "]"+"[Y="+y+"]");
+    }
+}
