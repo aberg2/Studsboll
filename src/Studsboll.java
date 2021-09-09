358f9e66a25c4f532be99398200473eadd621950
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-09-09 12:41:23 +0200

    🐱‍👤 Hittade en bra lösning

--- src/Studsboll.java
+++ src/Studsboll.java
@@ -4,8 +4,35 @@
  * Skriven av coolingen
  */
 public class Studsboll {
-    public static void main(String[] args) {
-    double deltaT = 0.1;
+    public static void main(String[] args) {double deltaT = 0.1;
+        double a = -9.82;
+        double v = 0;
+        double y = 10;
+        double x = 10;
+        double t = 0;
+        double e = 0.9;
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(t + ";" + y);
+            t += deltaT;
+            if (y < 0) {
+                v = -v;
+            } else {
+                v = updateVel(deltaT, a,v);
+            }
+            y = updatePos(deltaT, v, y);
+        }
+    }
+    private static double updatePos(double deltaT, double v, double y) {
+        y += v * deltaT;
+        return y;
+    }
+    private static double updateVel(double deltaT, double a, double v){
+        v += a + deltaT;
+        return v;
+    }
+}
+/*    double deltaT = 0.1;
     double a = -9.82;
     double v = 0;
     double y = 10;
@@ -37,4 +64,4 @@ public class Studsboll {
             }
         }
     }
-}
+}*/
